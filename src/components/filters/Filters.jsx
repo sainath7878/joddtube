a6752b2ce4f100dcb893392a378d6c4c5682bea7
Sainath <svmsainath@ymail.com>
2022-05-06 13:31:59 +0530

    Feat: Added single Video Page and Filters

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,101 +1,27 @@
 import styles from "components/filters/filters.module.css";
-import { useState } from "react";
+import { useFetch } from "hooks/useFetch";
 
-function Filters() {
-  const [filters, setFilters] = useState({
-    all: true,
-    valorant: false,
-    gta: false,
-    csgo: false,
-    bgmi: false,
-  });
+function Filters({ filters, setFilters }) {
+  const { categories } = useFetch("/api/categories");
+  console.log(categories);
   return (
     <div className={styles.filters}>
-      <button
-        className={`btn  ${
-          filters.all ? "btn-secondary" : `${styles.btnSecondaryOutline}`
-        } ${styles.btn}`}
-        onClick={() =>
-          setFilters({
-            ...filters,
-            all: true,
-            valorant: false,
-            gta: false,
-            csgo: false,
-            bgmi: false,
-          })
-        }
-      >
-        All
-      </button>
-      <button
-        className={`btn  ${
-          filters.valorant ? "btn-secondary" : `${styles.btnSecondaryOutline}`
-        } ${styles.btn}`}
-        onClick={() =>
-          setFilters({
-            ...filters,
-            valorant: true,
-            all: false,
-            gta: false,
-            csgo: false,
-            bgmi: false,
-          })
-        }
-      >
-        Valorant
-      </button>
-      <button
-        className={`btn  ${
-          filters.gta ? "btn-secondary" : `${styles.btnSecondaryOutline}`
-        } ${styles.btn}`}
-        onClick={() =>
-          setFilters({
-            ...filters,
-            gta: true,
-            valorant: false,
-            all: false,
-            csgo: false,
-            bgmi: false,
-          })
-        }
-      >
-        GTA V
-      </button>
-      <button
-        className={`btn  ${
-          filters.csgo ? "btn-secondary" : `${styles.btnSecondaryOutline}`
-        } ${styles.btn}`}
-        onClick={() =>
-          setFilters((prev) => ({
-            ...prev,
-            csgo: true,
-            all: false,
-            valorant: false,
-            gta: false,
-            bgmi: false,
-          }))
-        }
-      >
-        CS GO
-      </button>
-      <button
-        className={`btn  ${
-          filters.bgmi ? "btn-secondary" : `${styles.btnSecondaryOutline}`
-        } ${styles.btn}`}
-        onClick={() =>
-          setFilters((prev) => ({
-            ...prev,
-            bgmi: true,
-            all: false,
-            valorant: false,
-            gta: false,
-            csgo: false,
-          }))
-        }
-      >
-        BGMI
-      </button>
+      {categories &&
+        categories.map(({ _id, categoryName }) => {
+          return (
+            <button
+              className={`btn  ${
+                filters === categoryName
+                  ? "btn-secondary"
+                  : `${styles.btnSecondaryOutline}`
+              } ${styles.btn}`}
+              key={_id}
+              onClick={() => setFilters(categoryName)}
+            >
+              {categoryName}
+            </button>
+          );
+        })}
     </div>
   );
 }
